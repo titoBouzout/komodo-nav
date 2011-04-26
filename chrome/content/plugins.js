9de66f22c2158b0394233ea8b4c04d4d60ea00b0
tito.bouzout@gmail.com <tito@.(none)>
2011-04-26 00:39:33 -0300

    Updates registration of plugins and add a new plugin "Find.."

--- chrome/content/plugins.js
+++ chrome/content/plugins.js
@@ -10,12 +10,18 @@ if(typeof(extensions.navPlugins) == 'undefined' && typeof(extensions.nav) != 'un
 	
 	//register the actions of the menuitem to allow other add-ons extend the menus
 	
-	extensions.nav.registerPlugin('open_os', function(aPath){ extensions.nav.launch(aPath) });
-	extensions.nav.registerPlugin('open_komodo', function(aPath){ extensions.nav.openURL(aPath, true) });
+	extensions.nav.registerPlugin('open_os', 'Open with OS', function(aPath){ extensions.nav.launch(aPath) });
+	extensions.nav.registerPlugin('open_komodo', 'Open in a new tab', function(aPath){ extensions.nav.openURL(aPath, true) });
+	extensions.nav.registerPlugin('find', 'Find...', function(aPath)
+										  {
+											ko.launch.findInFiles.apply(ko.launch, ['', aPath]);
+										  }
+								  );
 	
 	/*
 		extensions.nav.registerPlugin(
 									  'the_name_of_my_action',
+									  'the_label_of_my_action',
 										function(aSelectedPath){
 										  extensions.myExt.myFunction(aSelectedPath)
 										}
